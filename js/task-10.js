01d803b960f68087bb736a44440ca031f62b7bf7
batalova-kira <batalovakira@gmail.com>
2023-08-02 00:41:58 +0300

    Update task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,6 +1,5 @@
 //Створюємо об'єкт елементів
 const elements = {
-  div: document.querySelector('#controls'),
   input: document.querySelector('input'),
   btnCreate: document.querySelector('button[data-create]'),
   btnDestroy: document.querySelector('button[data-destroy]'),
@@ -8,31 +7,32 @@ const elements = {
 }
 
 //Додаємо слухаччів на події
-elements.btnCreate.addEventListener('click', createBoxes);
+elements.btnCreate.addEventListener('click', () => {
+  const amount = Number(elements.input.value);
+  createBoxes(amount);
+});
 elements.btnDestroy.addEventListener('click', destroyBoxes);
 
-//Функція створення боксів - не можу збудувати логику ... ((
+//Функція створення боксів
 function createBoxes(amount) {
-  // змінна для створення боксів
-  let boxesCreate = () => creareBoxes(Number(elements.input.value));
-  // вимога створення боксів, якщо введено число >1 i <100
-  if (boxesCreate > 1 && boxesCreate < 100) {
-    const divEl = document.createElement("div");
-    divEl.setAttribute('id', 'box');
-    divEl.style.width = squareEl.style.height = '30px';
-    divEl.style.backgroundColor = 'teal';
-  }
+  let size = 30;
+  const arr = [];
   for (let i = 0; i < amount; i += 1) {
-  // збыльшення розмірів ...
-  divEl.style.width = squareEl.clientWidth + 10 + 'px';
-  divEl.style.height = squareEl.clientHeight + 10 + 'px';
-  
-}
+    size += 10;
+    const div = document.createElement('div');
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    arr.push(div);
+  }
+  elements.divBoxes.append(...arr);
+  elements.input.value = '';
 }
 
-//Функція створення боксів
+//Функція очищення боксів
 function destroyBoxes() {
-  elements.divBoxes.innerHTML = "";
+  elements.divBoxes.innerHTML = '';
+  elements.input.value = 0;
 }
 
 function getRandomHexColor() {
